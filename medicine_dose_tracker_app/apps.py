3856d7c74b49dafcd92d23291f9271f14f4f2cf7
akash <akashshere19@gmail.com>
2023-08-01 12:41:09 +0530

    add first commit

---
+++ medicine_dose_tracker_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MedicineDoseTrackerAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'medicine_dose_tracker_app'
